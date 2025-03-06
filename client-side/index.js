83af90bc4c54efc25dc3d1cfb0f492e1521d10dd
Vegybin <Vegybin@gmail.com>
2025-03-07 04:39:55 +0530

    control

--- client-side/index.js
+++ client-side/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import "dotenv/config";
 
 const app = express();
 const PORT = 3000;
@@ -11,6 +12,10 @@ const __dirname = path.dirname(__filename);
 
 // Serve static files from 'public' directory
 app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/server-ip", (req, res) => {
+    return res.send(process.env.SERVER_IP);
+})
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
