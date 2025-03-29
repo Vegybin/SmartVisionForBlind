01ec6c29f17817d738caec9ff89909ce147b1a17
Vegybin <Vegybin@gmail.com>
2025-03-29 21:17:22 +0530

    comfort

--- server-side/app.py
+++ server-side/app.py
@@ -93,6 +93,31 @@ def identify_face():
     return Response(audio_data, mimetype="audio/mpeg")
 
 
+@app.route('/caption-image', methods=['POST'])
+def process_image():
+
+    # Get the image file from the request
+    file = request.files.get('image')
+    if not file:
+        return {"error": "No image provided"}, 400
+
+    # Convert the uploaded image to a PIL Image
+    image = Image.open(io.BytesIO(file.read())).convert("RGB")  # Convert to RGB format
+
+    # Process the image using YOLO
+    current_caption = image_stuff.get_caption(image)
+    tts = gTTS(text=current_caption, lang='en')
+    rand_int = str(random.randint(0,999999))
+    tts.save("output"+rand_int+".mp3")
+
+    # Read the saved file and stream it
+    with open("output"+rand_int+".mp3", "rb") as f:
+        audio_data = f.read()
+    os.remove("output"+rand_int+".mp3")
+
+    return Response(audio_data, mimetype="audio/mpeg")
+
+
 @app.route('/speech-to-text', methods=['POST'])
 def speech_to_text():
     file = request.files.get('audio')
